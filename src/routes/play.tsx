163c4a121a6733cc18d1ed2eb12df3bbdb65f67d
Rendy Arya Kemal <renrror@gmail.com>
2023-09-24 11:40:24 +0700

    Dynamically load song based on path

--- src/routes/play.tsx
+++ src/routes/play.tsx
@@ -2,14 +2,24 @@ import { Route } from "@tanstack/react-router";
 import { rootRoute } from "./root";
 import GameContextProvider from "../components/GameScreen/GameContextProvider";
 import Playfield from "../components/GameScreen/Playfield";
-import { LAGTRAIN } from "../constants";
+import { CONFLICT, LAGTRAIN } from "../constants";
+
+const fakedb = [CONFLICT, LAGTRAIN];
 
 export const playRoute = new Route({
   getParentRoute: () => rootRoute,
-  path: "play",
-  component: () => (
-    <GameContextProvider song={LAGTRAIN}>
-      <Playfield />
-    </GameContextProvider>
-  ),
+  path: "play/$songId",
+  loader: async ({ params: { songId } }) => {
+    return fakedb[Number(songId)];
+  },
+
+  component: ({ useLoader }) => {
+    const song = useLoader();
+
+    return (
+      <GameContextProvider song={song}>
+        <Playfield />
+      </GameContextProvider>
+    );
+  },
 });
